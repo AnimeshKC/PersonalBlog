6645ef6495786c13233dbbba17bb60ef9c350945
AnimeshKC <animesh@ualberta.ca>
2020-05-27 17:17:35 -0600

    progress in previewing content

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@ const path = require("path")
 
 const { createFilePath } = require("gatsby-source-filesystem")
 const PostTemplate = path.resolve("./src/templates/postTemplate.js")
+const BlogTemplate = path.resolve("./src/templates/blog-template.js")
 // You can delete this file if you're not using it
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -42,6 +43,26 @@ exports.createPages = async ({ graphql, actions }) => {
       component: PostTemplate,
       context: {
         slug: post.fields.slug,
+      },
+    })
+  })
+  posts.forEach((_, index, postsArr) => {
+    const totalPages = postsArr.length
+    const postsPerPage = 1
+    const currentPage = index + 1
+    const isFirstPage = index === 0
+    const isLastPage = currentPage === totalPages
+
+    createPage({
+      path: isFirstPage ? "/index" : `/blog/${currentPage}`,
+      component: BlogTemplate,
+      content: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        isFirstPage,
+        isLastPage,
+        currentPage,
+        totalPages,
       },
     })
   })
