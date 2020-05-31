57457f0e56a735d2d3a489bde08d4f22ed1a399f
AnimeshKC <animesh@ualberta.ca>
2020-05-31 15:40:03 -0600

    added disqus comments

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -1,21 +1,31 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
-
+import { DiscussionEmbed } from "disqus-react"
 //use of dangerouslySetInnerHTML is fine in this instance since it doesn't rely on user input
-const PostTemplate = ({ data: post }) => (
-  <Layout>
-    <div>
-      <h1>{post.markdownRemark.frontmatter.title} </h1>
-      <h6>
-        Last updated -{" "}
-        {post.markdownRemark.frontmatter.updated ||
-          post.markdownRemark.frontmatter.date}
-      </h6>
-      <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }} />
-    </div>
-  </Layout>
-)
+const PostTemplate = ({ data: post }, pageContext) => {
+  const baseUrl = "https://blog.animeshkc.me/"
+  const disqusShortname = "https-blog-animeshkc-me"
+  const disqusConfig = {
+    identifier: post.markdownRemark.id,
+    title: post.markdownRemark.frontmatter.title,
+    url: baseUrl + pageContext.slug,
+  }
+  return (
+    <Layout>
+      <div>
+        <h1>{post.markdownRemark.frontmatter.title} </h1>
+        <h6>
+          Last updated -{" "}
+          {post.markdownRemark.frontmatter.updated ||
+            post.markdownRemark.frontmatter.date}
+        </h6>
+        <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }} />
+      </div>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query($slug: String!) {
